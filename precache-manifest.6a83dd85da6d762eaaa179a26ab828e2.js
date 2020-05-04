6bd02c17cb6f428c2b54bb8845c7c0d8c4c4c114
Eslam Elfateh <eslam.elfateh.94@gmail.com>
2020-05-04 03:03:28 +0200

    Updates

---
+++ precache-manifest.6a83dd85da6d762eaaa179a26ab828e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d1db5ec9d274efed86325d70bc799cd",
+    "url": "/monester/index.html"
+  },
+  {
+    "revision": "a617f0c6eb5dd0a9388e",
+    "url": "/monester/static/css/main.dfada2d6.chunk.css"
+  },
+  {
+    "revision": "f6d2ab47bb337dc705a8",
+    "url": "/monester/static/js/2.b67fbdca.chunk.js"
+  },
+  {
+    "revision": "a617f0c6eb5dd0a9388e",
+    "url": "/monester/static/js/main.da9ffc05.chunk.js"
+  },
+  {
+    "revision": "dba01398d4f2fa6adb39",
+    "url": "/monester/static/js/runtime~main.190b0c6a.js"
+  }
+]);
